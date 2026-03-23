42d95180fc63efb7ef70b84a665fa7b11ca8a100
exgules <Respekt32113@mail.ru>
2026-03-23 16:26:54 +0400

    style compleate

--- my-shop-app/src/components/common/BreadCrumbs.jsx
+++ my-shop-app/src/components/common/BreadCrumbs.jsx
@@ -1,10 +1,15 @@
 // src/components/common/BreadCrumbs.jsx
-export default function BreadCrumbs() {
+export default function BreadCrumbs({ currentPage }) {
   return (
     <div className="bread-crumbs">
       <div className="vertical-line"></div>
       <div className="text-home">Home</div>
-      <div className="text-shop">Shop</div>
+      <div className={`text-shop ${currentPage === 'shop' ? 'active' : ''}`}>
+        Shop
+      </div>
+      {currentPage === 'cart' && (
+        <div className="text-cart active">Cart</div>
+      )}
     </div>
   );
 }
--- my-shop-app/src/components/shop/ShopHeader.jsx
+++ my-shop-app/src/components/shop/ShopHeader.jsx
@@ -10,9 +10,7 @@ const ShopHeader = () => {
         <div className="bread-crumbs">
           <div className="vertical-line"></div>
           <div className="text-home">Home</div>
-          <div className="page-shop">
           <div className="text-shop">Shop</div>
-          </div>
         </div>
         <div className="horizontal-line"></div>
       </div>
